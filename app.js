f8746453790820cea8dca94a633887a492a59210
masudrana <masudranaacb@gmail.com>
2022-03-05 11:51:32 +0600

    usnig clearInterval

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 console.log('frist')
-setInterval(()=>{
-    console.log('third')
+// setInterval(()=>{
+//     console.log('third')
+// },1000)
+let second=0
+const timeID=setInterval(()=>{
+    second++
+    console.log(second)
+    if(second>10){
+        clearInterval(timeID)
+    }
 },1000)
 console.log('seecond')
